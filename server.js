84a23a029740456dffb3996c5028c5bd9eaacb52
Riski_ <riskiafrizal11@gmail.com>
2026-05-18 22:17:51 +0700

    Remove server listening logic from server.js

--- server.js
+++ server.js
@@ -77,7 +77,4 @@ app.post('/api/report/odc', upload.fields([
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server aktif di port ${PORT}`);
-});
+module.exports = app;
